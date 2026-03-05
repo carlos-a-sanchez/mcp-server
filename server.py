7a27c6e1c9a26438a46f9892ff015e78042196b3
Carlos Sanchez <carlos.a.sanchez@outlook.com>
2026-03-05 12:34:40 -0600

    Initial commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("server") # ,port=8000, host="0.0.0.0"
+
+@mcp.tool()
+def greeting(name: str) -> str:
+    "Send a greeting"
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
+
+
+
