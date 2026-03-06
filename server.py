b746d26abdba46c75c470d11c71b040be9c21c0a
Carlos Sanchez <carlos.a.sanchez@outlook.com>
2026-03-05 19:00:54 -0600

    ip 0.0.0.0

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from mcp.server.fastmcp import FastMCP
 
-mcp = FastMCP("server") # ,port=8000, host="0.0.0.0"
+# mcp = FastMCP("server") # ,port=8000, host="0.0.0.0"
+mcp = FastMCP("server", port=8000, host="0.0.0.0")
+
 
 @mcp.tool()
 def greeting(name: str) -> str:
