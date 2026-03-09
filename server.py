35180b7c1bb893b60a62f05a59c5c827adfda970
Carlos Sanchez <carlos.a.sanchez@outlook.com>
2026-03-09 12:31:45 -0600

    GLN param name

--- server.py
+++ server.py
@@ -8,9 +8,9 @@ mcp = FastMCP("server", port=8000, host="0.0.0.0")
 
 
 @mcp.tool()
-def find_in_hanna(name: str) -> str:
+def find_in_hanna(gln: str) -> str:
     "Query GLN data in Hanna"
-    return f"Your entrance: [{name}]"
+    return f"Your entrance: [{gln}]"
 
 if __name__ == "__main__":
     mcp.run(transport="streamable-http")
